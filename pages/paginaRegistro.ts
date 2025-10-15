34ab13f4407c8ea8b3be3d4ed1f5bbb5b825a135
deissyherreralilab <deissy.herrera@lilab.pe>
2025-10-14 21:34:59 -0500

    pruebas de registro 6 tc

--- pages/paginaRegistro.ts
+++ pages/paginaRegistro.ts
@@ -1,13 +1,60 @@
-import {Page, Locator} from '@playwright/test';
+//importaciones de Playwrright
+import { Page, Locator } from '@playwright/test';
 
-export class PaginarRegistro{
+//Exponiendo la PaginaRegistro para poder usarla en los tests
+export class PaginarRegistro {
+    //Siempre necesitamos el page para interactuar con la pagina
     readonly page: Page;
+    //Definimos los localizadores que vamos a usar con la propiedad locator
     readonly nombreInput: Locator;
+    readonly apellidoInput: Locator;
+    readonly emailInput: Locator;
+    readonly contrasenaInput: Locator;
+    readonly botonRegisrarse: Locator;
+    readonly botonIniciarSesion: Locator;
+    //variables de  textos
+    readonly mensajeDeCreacionDeCuenta: string;
+    readonly mensajeEmailUtilizado: string;
 
-    constructor(page: Page){
+
+    //Constructor que recibe el page y define los localizadores
+    constructor(page: Page) {
+        //cuerpo del metodo, lo que se ejcutara cuadno se llame el metodo
+        //asignamos el page a la propiedad de la clase
         this.page = page;
+        //Definimos el localizador para el campo nombre
         this.nombreInput = page.getByRole('textbox', { name: 'Nombre' });
+        this.apellidoInput = page.locator('[name="lastName"]');
+        this.emailInput = page.getByRole('textbox', { name: 'Correo electrónico' });
+        this.contrasenaInput = page.getByRole('textbox', { name: 'Contraseña' });
+        this.botonRegisrarse = page.getByTestId('boton-registrarse');
+
+        this.mensajeDeCreacionDeCuenta = "Registro exitoso!"
+        this.mensajeEmailUtilizado = "Email already in use"
     }
+
+    //clase de tipo asincrona
+    async visitarPaginaRegistro(){
+        await this.page.goto('http://localhost:3000/signup');
+        await this.page.waitForLoadState('domcontentloaded')
+    }
+
+    async completarFormularioRegistro(nombre: string, apellido: string, email: string, contraseña: string){
+        await this.nombreInput.fill(nombre);
+        await this.apellidoInput.fill(apellido)
+        await this.emailInput.fill(email)
+        await this.contrasenaInput.fill(contraseña);        
+    }
+
+    async hacerClickBotonRegistro(){
+        await this.botonRegisrarse.click()
+    }
+
+    async registrarUsuario(nombre: string, apellido: string, email: string, contraseña: string){
+        await this.completarFormularioRegistro(nombre,apellido,email,contraseña)
+        await this.hacerClickBotonRegistro();
+    }
+
 }
 
 
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test';
-import { PaginarRegistro } from '../pages/paginaRegistro';
+/*import { PaginarRegistro } from '../pages/paginaRegistro';
 
 let paginaRegistro: PaginarRegistro;
 
@@ -7,26 +7,87 @@ test('TC1 - registro exitoso', async ({ page }) => {
   paginaRegistro = new PaginarRegistro(page);
   const emailAleatorio = 'deluhedu' + Math.floor(Math.random() * 1000) + '@test.com';
 
-  await page.goto('http://localhost:3000/signup');+
-  await paginaRegistro.nombreInput.fill('Deissy')
-  await page.locator('[name="lastName"]').fill('Herrera');
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).fill(emailAleatorio);
-  await page.getByRole('textbox', { name: 'Contraseña' }).fill('Contraseña123');
-  await page.getByTestId('boton-registrarse').click();
-  await expect(page.getByText('Registro exitoso!')).toBeVisible();  
+  await paginaRegistro.visitarPaginaRegistro(); //accion de ir a visitar la URL
+  await paginaRegistro.registrarUsuario("Deissy", "Herrera", emailAleatorio, "contraseña123");
+  await expect(page.getByText(paginaRegistro.mensajeDeCreacionDeCuenta)).toBeVisible();  
   
 });
 
 test('TC2 - registro no exitoso, email existente', async ({ page }) => {
   paginaRegistro = new PaginarRegistro(page);
-  await page.goto('http://localhost:3000/signup');+
-  await page.getByRole('textbox', { name: 'Nombre' }).fill('Deissy');
-  await page.locator('[name="lastName"]').fill('Herrera');
-  await page.getByRole('textbox', { name: 'Correo electrónico' }).fill('deluhedu113@test.com');
-  await page.getByRole('textbox', { name: 'Contraseña' }).fill('Contraseña123');
-  await page.getByTestId('boton-registrarse').click();
-  await expect(page.getByText('Email already in use')).toBeVisible();  
+  await paginaRegistro.visitarPaginaRegistro();
+
+  await paginaRegistro.registrarUsuario("Deissy", "Herrera", "deluhedu113@test.com", "Contraseña123");
   
+  await expect(page.getByText(paginaRegistro.mensajeEmailUtilizado)).toBeVisible();  
+  
+});*/
+
+test('TC-1 - verificacion de elementos en la pagina de registro', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await expect(page.locator('input[name="firstName"]')).toBeVisible();
+  await expect(page.locator('input[name="lastName"]')).toBeVisible();
+  await expect(page.locator('input[name="email"]')).toBeVisible();
+  await expect(page.locator('input[name="password"]')).toBeVisible();
+  await expect(page.getByTestId('boton-registrarse')).toBeVisible();
+  //await page.waitForTimeout(5000);
+
+});
+
+test('TC-2 - verificar boton de registro esta inhabiliatdo por defecto', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await expect(page.getByTestId('boton-registrarse')).toBeDisabled();
+
+});
+
+test('TC-3 - verificar boton de registro se habilita al completar los campos obligatorios', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.locator('input[name="firstName"]').fill('Deissy')
+  await page.locator('input[name="lastName"]').fill('Herrera')
+  await page.locator('input[name="email"]').fill('deissytest'+Date.now().toString()+'@gmail.com') //email aleatorio
+  await page.locator('input[name="password"]').fill('12345678');
+  await expect(page.getByTestId('boton-registrarse')).toBeEnabled();
+
+});
+
+test('TC-4 - verificar redireccionamiento a pagina de inicio de sesion al hacer clic', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.getByTestId('boton-login-header-signup').click();
+  await expect(page).toHaveURL('http://localhost:3000/login');
+  await page.waitForTimeout(5000);  
+
+});
+
+
+test('TC-5 - verificar con datos validos registro exitoso', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.locator('input[name="firstName"]').fill('Deissy')
+  await page.locator('input[name="lastName"]').fill('Herrera')
+  await page.locator('input[name="email"]').fill('deissytest'+Date.now().toString()+'@gmail.com') //email aleatorio
+  await page.locator('input[name="password"]').fill('12345678');
+  await page.getByTestId('boton-registrarse').click();
+  await expect(page.getByText('Registro exitoso')).toBeVisible();  
+
+});
+
+test('TC-6 - verificar usuairo no pueda registrarse con email existente', async ({ page }) => {
+  const email = 'deissytest' + Date.now().toString() + '@gmail.com'
+  await page.goto('http://localhost:3000/');
+  await page.locator('input[name="firstName"]').fill('Deissy')
+  await page.locator('input[name="lastName"]').fill('Herrera')
+  await page.locator('input[name="email"]').fill(email) //email aleatorio
+  await page.locator('input[name="password"]').fill('12345678');
+  await page.getByTestId('boton-registrarse').click();
+  await expect(page.getByText('Registro exitoso')).toBeVisible();  
+  await page.goto('http://localhost:3000/');
+  await page.locator('input[name="firstName"]').fill('Deissy')
+  await page.locator('input[name="lastName"]').fill('Herrera')
+  await page.locator('input[name="email"]').fill(email) //email aleatorio
+  await page.locator('input[name="password"]').fill('12345678');
+  await page.getByTestId('boton-registrarse').click();
+  await expect(page.getByText('Email already in use')).toBeVisible();
+  await expect(page.getByText('Registro exitoso')).not.toBeVisible();    
+
 });
 
 
